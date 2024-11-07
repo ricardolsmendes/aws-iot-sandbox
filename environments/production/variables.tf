8dc1263b063a25e8ffe371885954c8f567dd7066
Ricardo Lopes de Souza Mendes <ricardolsmendes@gmail.com>
2024-11-07 15:28:37 -0300

    add qa and prod envs

---
+++ environments/production/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key" {
+  description = "The AWS access key."
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key."
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "The AWS region."
+  type        = string
+  default     = ""
+}
+
+variable "environment" {
+  description = "The deployment environment."
+  type        = string
+  default     = "prod"
+}
