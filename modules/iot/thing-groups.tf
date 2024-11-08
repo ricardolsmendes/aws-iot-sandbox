f2cc94c3d5261d98e3e7a7d977dc760be8663b59
Ricardo Mendes <ricardolsmendes@gmail.com>
2024-11-07 21:51:49 -0300

    feat: add environment suffix to the resource names (#2)

--- modules/iot/thing-groups.tf
+++ modules/iot/thing-groups.tf
@@ -1,35 +1,35 @@
 resource "aws_iot_thing_group" "industrial_devices" {
-  name = "industrial-devices"
+  name = "industrial-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "logistics_devices" {
-  name = "logistics-devices"
+  name = "logistics-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "medical_devices" {
-  name = "medical-devices"
+  name = "medical-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "personal_transportation_devices" {
-  name = "personal-transportation-devices"
+  name = "personal-transportation-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "point_of_sale_devices" {
-  name = "point-of-sale-devices"
+  name = "point-of-sale-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "security_devices" {
-  name = "security-devices"
+  name = "security-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "sensors" {
-  name = "sensors"
+  name = "sensors-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "smart_home_devices" {
-  name = "smart-home-devices"
+  name = "smart-home-devices-${var.environment}"
 }
 
 resource "aws_iot_thing_group" "smart_wearables" {
-  name = "smart-wearables"
+  name = "smart-wearables-${var.environment}"
 }
--- modules/iot/thing-types.tf
+++ modules/iot/thing-types.tf
@@ -1,46 +1,46 @@
 resource "aws_iot_thing_type" "air_quality_sensor" {
-  name = "air-quality-sensor"
+  name = "air-quality-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "barcode_scanner" {
-  name = "barcode-scanner"
+  name = "barcode-scanner-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "employee_posture_monitor" {
-  name = "employee-posture-monitor"
+  name = "employee-posture-monitor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "humidity_sensor" {
-  name = "humidity-sensor"
+  name = "humidity-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "noise_level_monitor" {
-  name = "noise-level-monitor"
+  name = "noise-level-monitor-${var.environment}"
 }
 resource "aws_iot_thing_type" "noise_level_sensor" {
-  name = "noise-level-sensor"
+  name = "noise-level-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "oil_analysis_sensor" {
-  name = "oil-analysis-sensor"
+  name = "oil-analysis-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "pressure_sensor" {
-  name = "pressure-sensor"
+  name = "pressure-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "proximity_sensor" {
-  name = "proximity-sensor"
+  name = "proximity-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "temperature_sensor" {
-  name = "temperature-sensor"
+  name = "temperature-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "vibration_sensor" {
-  name = "vibration-sensor"
+  name = "vibration-sensor-${var.environment}"
 }
 
 resource "aws_iot_thing_type" "water_quality_sensor" {
-  name = "water-quality-sensor"
+  name = "water-quality-sensor-${var.environment}"
 }
